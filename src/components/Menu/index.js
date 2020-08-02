08af024443271be3e1a041d40b87e7837fca8dbf
marianacoimbra <mariana.coimbra@ccc.ufcg.edu.br>
2020-08-01 20:49:59 -0400

    first commit

---
+++ src/components/Menu/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import jujubaFlixLogo from "../../assets/jujubaFlixLogo.png";
+import "./style.css";
+import Button from "../Button/index";
+const Menu = () => {
+  return (
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={jujubaFlixLogo} alt="jujubaFlix Logo" />
+      </a>
+      <Button as="a" className="ButtonLink" href="/">
+        Novo conteudo
+      </Button>
+    </nav>
+  );
+};
+
+export default Menu;
